11f316ceab02718babde96568387b05e4784c03f
ahmuzc <89759644+ahmuzc@users.noreply.github.com>
2023-05-15 16:29:13 +0800

    Update refractory_septic_shock_plasmapheresis.sql

--- refractory_septic_shock_plasmapheresis.sql
+++ refractory_septic_shock_plasmapheresis.sql
@@ -1,5 +1,15 @@
-select distinct patientunitstayid from eicu.sepsis_adult_eicu where patientunitstayid in
-(select distinct patientunitstayid from eicu.norepinephrine_info_eicu where rate_norepinephrine>0.4)
-AND patientunitstayid in 
-(select distinct patientunitstayid from eicu.treatment where treatmentstring like '%plasmapheresis%'
-OR treatmentstring like '%fresh frozen plasma%')
+create table trt as
+select * from eicu.treatment where 
+lower(treatmentstring) like '%plasmapheresis%' or
+lower(treatmentstring) like '%plasma exchange%' or
+lower(treatmentstring) like '%fresh frozen plasma%'
+
+create table dx as
+select * from eicu.diagnosis where 
+patientunitstayid in (select distinct patientunitstayid from trt) AND
+(lower(diagnosisstring) like '%severe sepsis%' 
+or lower(diagnosisstring) like '%septic shock%'
+or lower(diagnosisstring) like '%sepsis|severe%')
+
+select * from eicu.patient where 
+patientunitstayid in (select distinct patientunitstayid from dx)
